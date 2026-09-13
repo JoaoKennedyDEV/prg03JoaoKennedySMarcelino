f04a42c2f1f17c11a2d4d0b7eeee0f1050bcc110
JoaoKennedyDEV <joaokennedysantosmacelino@gmail.com>
2026-09-13 13:44:02 -0300

    adicionar classe usuario

---
+++ prg03joaokennedysmarcelino/src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -0,0 +1,21 @@
+package br.com.ifba.usuario.entity;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kennedy
+ */
+public class Usuario {
+    public String nome;
+    public String cpf;
+    public String genero;
+    public String dataNascimento;
+    public String telefone;
+    public String email;
+    public String login;
+    public String senha;
+}
